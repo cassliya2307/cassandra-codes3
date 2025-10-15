9ed13bb76ef1c1ac8151bc4087649f9fd24a1cad
cassliya2307 <haliyakareem23@gmail.com>
2025-10-15 05:48:05 +0100

    Add Task.java

---
+++ Task.java
@@ -0,0 +1,118 @@
+pudlic class Task{
+
+public static int even Numbers(int num){
+for(int count = 1; count <= 100; count++){
+if(count % 2 == 0){
+System.out.print(count);
+}
+}
+
+
+
+}
+
+
+public static int oddNumbers(int num){
+for(count = 50; count <= 100; count++){
+if(count % 2 != 0){System.out.print(count);}
+
+
+}
+
+}
+
+
+public static int decrease(int num){
+for(count = 100; count >= 1; count--){
+
+System.out.print(count);
+
+
+}
+
+return 0;
+}
+
+
+public static int square(int num){
+for(count = 1; count <= 20; count++){
+
+System.out.print(count * count);
+
+}
+return 0;
+}
+
+
+public static int multipleOfThree(int num){
+for(count = 1; count <= 50; count++){
+if(count % 3 == 0){System.out.print(count);
+}
+}
+
+return 0;
+
+}
+
+public static int multipleOfThreeAndFive(int num){
+for(count = 1; count <= 100; count++){
+if(count % 3 == 0 && count % 5 == 0){System.out.print(count);
+}
+}
+
+return 0;
+
+
+}
+
+
+public static int multipleOfSeven(int num){
+for(count = 1; count <= 100; count++){
+if(count % 7 == 0){System.out.print(count);
+}
+}
+
+return 0;
+
+
+}
+
+
+public static int sumOfNumbers(int num){
+sum = 0;
+for(count = 1; count <= 50; count++){
+sum += count;
+
+}
+
+return sum
+
+
+
+}
+
+
+public static int productOfNumbers(int num){
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
